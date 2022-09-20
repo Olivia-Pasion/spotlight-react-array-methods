6a8195795906746beafd67be1a613fe67ff8ffc7
olivia pasion <oap9820@gmail.com>
2022-09-20 12:02:53 -0700

    filterYoungDogs in filter.js passes appropriate test

--- filter.js
+++ filter.js
@@ -18,7 +18,10 @@ export const filterAu = (countries) => {
 // INPUT: the array of dogs from data.js
 // OUTPUT: only dogs that are 2 years old or younger
 // REQS: use .filter
-export const filterYoungDogs = (dogs) => {};
+export const filterYoungDogs = (dogs) => {
+  const youngDog = dogs.filter((dog) => (dog.age <= 2));
+  return youngDog;
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: a list of names of dogs that are 2 years old or younger
