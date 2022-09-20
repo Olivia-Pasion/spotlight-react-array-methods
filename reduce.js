f67462159710693ee437fdfd5536933c7c73f407
olivia pasion <oap9820@gmail.com>
2022-09-20 12:57:49 -0700

    working on getAverageAge in reduce.js

--- reduce.js
+++ reduce.js
@@ -10,4 +10,8 @@ export const getSneaks = (brands) => {
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  function avg({ age }) {
+    
+  }
+};
