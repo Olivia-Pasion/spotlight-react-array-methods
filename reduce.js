4201e91381392b69e3382cada085ff9e43fb8b1c
olivia pasion <oap9820@gmail.com>
2022-09-20 12:44:35 -0700

    getSneaks in reduce.js passes appropriate test

--- reduce.js
+++ reduce.js
@@ -2,7 +2,10 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  const shoeNames = brands.reduce((previousValue, currentValue) => [...previousValue, ...currentValue.shoes], []);
+  return shoeNames;
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
